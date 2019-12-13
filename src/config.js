992a1ee52fac70b66c647b408f634634bfaf593a
zottwickel <scott.g.mckell@gmail.com>
2019-12-13 11:50:07 -0700

    ready for deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://scott:foobar@localhost/blogful'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://scott:foobar@localhost/blogful'
 }
